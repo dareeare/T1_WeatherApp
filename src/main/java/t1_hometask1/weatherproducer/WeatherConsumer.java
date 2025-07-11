b65617b27ded28919a61baeffbb4a2bf88891938
dareeare <voitehovsk.daria@gmail.com>
2025-07-11 18:44:47 +0300

    Update WeatherConsumer.java

--- src/main/java/t1_hometask1/weatherproducer/WeatherConsumer.java
+++ src/main/java/t1_hometask1/weatherproducer/WeatherConsumer.java
@@ -1,4 +1,17 @@
 package t1_hometask1.weatherproducer;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class WeatherConsumer {
+
+    @KafkaListener(topics = "weather-topic", groupId = "weather-group")
+    public void consume(WeatherData weatherData) {
+        System.out.println("Consumed weather data: " + weatherData);
+
+        if (weatherData.getCondition().equals("Rainy")) {
+            System.out.println("ALERT: Rain detected in " + weatherData.getCity());
+        }
+    }
 }
